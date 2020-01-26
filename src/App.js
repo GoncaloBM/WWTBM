e0b0123cd7ad07db1f911d8c9dfc1211de196d91
Gonçalo Mira <goncalomira99@gmail.com>
2020-01-26 09:22:50 +0000

    Map done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import Question from './components/Question'
-import './App.css';
-
+import React, { Component } from "react";
+import "./styles.css";
+import { initialQuestion, shuffle, Question } from "./components/Question";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialQuestion();
+  }
+
+  getQuestionAndAnswers = () => {
+    this.setState({ isLoaded: false }, () => {
+      fetch(
+        "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple"
+      )
+        .then(response => response.json())
+        .then(data => {
+          let answersArray = [];
+          answersArray.push(data.results[0].correct_answer);
+          answersArray.push(data.results[0].incorrect_answers[0]);
+          answersArray.push(data.results[0].incorrect_answers[1]);
+          answersArray.push(data.results[0].incorrect_answers[2]);
+
+          let randomAnswers = shuffle(answersArray);
+
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            question: data.results[0].question,
+            correctAnswer: data.results[0].correct_answer,
+            answers: randomAnswers,
+            isLoaded: true
+          });
+        });
+    });
+  };
+
+  checkVictory = input => {
+    console.log(this.state.answers[input]);
+
+    if (this.state.answers[input] === this.state.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  getNextState = input => {
+    let selectedAnswer = this.state.answers[input];
+    console.log(selectedAnswer);
+    let questionRight = this.checkVictory(input);
+    if (questionRight) {
+      this.getQuestionAndAnswers();
+    } else {
+      this.setState({
+        messagem: "Perdeste!"
+      });
+    }
+  };
+
   render() {
-    return (
-      <Question />
-    )
-}
+    if (this.state.question === null) {
+      return null;
+    } else {
+      return (
+        <div className="App">
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start editing to see some magic happen!</h2>
+          <button
+            onClick={() => {
+              this.getQuestionAndAnswers();
+            }}
+          >
+            Clica aqui
+          </button>
+          <div>Question number: {this.state.activeQuestion}</div>
+          <div id="question-screen">
+            <div id="question">{this.state.question}</div>
+
+            <Question
+              answers={this.state.answers}
+              clickCallback={index => {
+                this.getNextState(index);
+              }}
+            />
+
+            <div>Correct : {this.state.correctAnswer}</div>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,272 +1,56 @@
-import React, { Component } from "react";
-import decoding from "./DecodingFunction";
-import "./Question.css";
+import React from "react";
 
-class Question extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      question: [],
-      answers: [],
-      answered1: false,
-      answered2: false,
-      answered3: false,
-      answered4: false,
-      correct1: false,
-      correct2: false,
-      correct3: false,
-      correct4: false,
-      correct: false,
-      amount_corrected: 0
-    };
+export const initialQuestion = () => {
+  return {
+    activeQuestion: 0,
+    messagem: "",
+    answers: [],
+    question: "",
+    correctAnswer: "",
+    isLoaded: false
+  };
+};
+
+export const shuffle = arr => {
+  var i, j, temp;
+  for (i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
+  return arr;
+};
 
-  getQuestion = () => {
-    this.setState({ isLoaded: false }, () => {
-      fetch(
-        "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple"
-      )
-        .then(response => response.json())
-        .then(data => {
-          this.setState({
-            question: data.results[0],
-            isLoaded: true
-          });
-          this.getAnswers();
-        });
-    });
-  };
+export const checkVictory = (input, state) => {
+  let currentState = { ...state };
+  console.log(state);
 
-  getAnswers = () => {
-    let answersArr = [];
-    answersArr.push(this.state.question.correct_answer);
-    answersArr.push(this.state.question.incorrect_answers[0]);
-    answersArr.push(this.state.question.incorrect_answers[1]);
-    answersArr.push(this.state.question.incorrect_answers[2]);
+  if (currentState.answers[input] === currentState.correctAnswer) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-    let randomArr = this.shuffle(answersArr);
-
-    console.log(randomArr);
-    this.setState({
-      answers: [randomArr[0], randomArr[1], randomArr[2], randomArr[3]]
-    });
-  };
-
-  shuffle = arr => {
-    var i, j, temp;
-    for (i = arr.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-    return arr;
-  };
-
-  toggleAnswered = index => {
-    if (index === 0) {
-      let currentState1 = this.state.answered1;
-      this.setState({ answered1: !currentState1 });
-    } else if (index === 1) {
-      let currentState2 = this.state.answered2;
-      this.setState({ answered2: !currentState2 });
-    } else if (index === 2) {
-      let currentState3 = this.state.answered3;
-      this.setState({ answered3: !currentState3 });
-    } else if (index === 3) {
-      let currentState4 = this.state.answered4;
-      this.setState({ answered4: !currentState4 });
-    }
-
-    for (let i = 0; i < 4; i++) {
-      this.checkCorrectAnswer(i);
-    }
-  };
-
-  checkCorrectAnswer = index => {
-    if (index === 0) {
-      if (this.state.answers[index] === this.state.question.correct_answer) {
-        this.setState({ correct1: !this.state.correct1 });
-      }
-    } else if (index === 1) {
-      if (this.state.answers[index] === this.state.question.correct_answer) {
-        this.setState({ correct2: !this.state.correct2 });
-      }
-    } else if (index === 2) {
-      if (this.state.answers[index] === this.state.question.correct_answer) {
-        this.setState({ correct3: !this.state.correct3 });
-      }
-    } else if (index === 3) {
-      if (this.state.answers[index] === this.state.question.correct_answer) {
-        this.setState({ correct4: !this.state.correct4 });
-      }
-    }
-  };
-
-  onClickAnswer1 = () => {
-    this.resetStates();
-    this.getQuestion();
-    if (this.state.answers[0] === this.state.question.correct_answer) {
-      this.setState(prevState => {
-        return { amount_corrected: prevState.amount_corrected + 1 };
-      });
-    }
-  };
-
-  onClickAnswer2 = () => {
-    this.resetStates();
-    this.getQuestion();
-    if (this.state.answers[1] === this.state.question.correct_answer) {
-      this.setState(prevState => {
-        return { amount_corrected: prevState.amount_corrected + 1 };
-      });
-    }
-  };
-
-  onClickAnswer3 = () => {
-    this.resetStates();
-    this.getQuestion();
-    if (this.state.answers[2] === this.state.question.correct_answer) {
-      this.setState(prevState => {
-        return { amount_corrected: prevState.amount_corrected + 1 };
-      });
-    }
-  };
-
-  onClickAnswer4 = () => {
-    this.resetStates();
-    this.getQuestion();
-    if (this.state.answers[3] === this.state.question.correct_answer) {
-      this.setState(prevState => {
-        return { amount_corrected: prevState.amount_corrected + 1 };
-      });
-    }
-  };
-
-  resetStates = () => {
-    this.setState({
-      answered1: false,
-      answered2: false,
-      answered3: false,
-      answered4: false,
-      correct1: false,
-      correct2: false,
-      correct3: false,
-      correct4: false
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button
-          onClick={() => {
-            this.getQuestion();
-          }}
-        >
-          Generate Question
-        </button>
-        {this.state.isLoaded ? (
-          <div className="screen-question">
-            <div className="full-question">
-              <div className="arrow-left" />
-              <div className="question">
-                {decoding(this.state.question.question)}
-              </div>
-              <div className="arrow-right" />
-            </div>
-
-            <div className="answers">
-              <div
-                className="full-answer"
-                onClick={() => {
-                  this.toggleAnswered(0);
-                  setTimeout(this.onClickAnswer1, 3000);
-                }}
-              >
-                <div className="arrow-left" />
-                <div
-                  className={
-                    this.state.correct1
-                      ? "correct"
-                      : `${this.state.answered1 ? "answered" : "answer"}`
-                  }
-                >
-                  {decoding(this.state.answers[0])}
-                </div>
-                <div className="arrow-right" />
-              </div>
-
-              <div
-                className="full-answer"
-                onClick={() => {
-                  this.toggleAnswered(1);
-                  setTimeout(this.onClickAnswer3, 3000);
-                  // this.getQuestion();
-                }}
-              >
-                <div className="arrow-left" />
-                <div
-                  className={
-                    this.state.correct2
-                      ? "correct"
-                      : `${this.state.correct2 ? "answered" : "answer"}`
-                  }
-                >
-                  {decoding(this.state.answers[1])}
-                </div>
-                <div className="arrow-right" />
-              </div>
-
-              <div
-                className="full-answer"
-                onClick={() => {
-                  this.toggleAnswered(2);
-                  setTimeout(this.onClickAnswer3, 3000);
-                  // this.getQuestion();
-                }}
-              >
-                <div className="arrow-left" />
-                <div
-                  className={
-                    this.state.correct3
-                      ? "correct"
-                      : `${this.state.answered3 ? "answered" : "answer"}`
-                  }
-                >
-                  {decoding(this.state.answers[2])}
-                </div>
-                <div className="arrow-right" />
-              </div>
-
-              <div
-                className="full-answer"
-                onClick={() => {
-                  this.toggleAnswered(3);
-                  setTimeout(this.onClickAnswer4, 3000);
-                  // this.getQuestion();
-                }}
-              >
-                <div className="arrow-left" />
-                <div
-                  className={
-                    this.state.correct4
-                      ? "correct"
-                      : `${this.state.answered4 ? "answered" : "answer"}`
-                  }
-                >
-                  {decoding(this.state.answers[3])}
-                </div>
-                <div className="arrow-right" />
-              </div>
-            </div>
-
-            {/* <div>Correct: {decoding(this.state.question.correct_answer)}</div> */}
+export const Question = props => {
+  return (
+    <div id="answers">
+      {props.answers.map((item, index) => {
+        return (
+          <div
+            className="answer"
+            key={index}
+            //onClick={index => {
+            //this.getNextState(index);
+            onClick={() => {
+              props.clickCallback(index);
+            }}
+          >
+            {item}
           </div>
-        ) : null}
-      </div>
-    );
-  }
-}
-
-export default Question;
+        );
+      })}
+    </div>
+  );
+};
